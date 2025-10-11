a3279b3d8a94c91bf1290d9a53a8c2044e483e49
Kevo669 <kday1206@terpmail.umd.edu>
2025-10-11 08:36:56 -0400

    Update library_name.py

--- src/library_name.py
+++ src/library_name.py
@@ -1,1 +1,23 @@
+# 1. Find_largest_transaction 
+# will sort transactions in order from largest to smallest by amount spent 
+def sort_largest_transaction(accounts):
+    unsorted_list = accounts.copy()
+    sorted_list = []
+    # sort until unsorted is done 
+    while unsorted_list:
+        largest_transaction = unsorted_list[0]
+        largest_amount = largest_transaction["Amount"]
 
+        # find transaction with largest amount
+        for transaction in unsorted_list:
+            if transaction["Amount"] > largest_amount:
+                largest_amount = transaction["Amount"]
+                largest_transaction = transaction 
+
+        # add the largest transaction to the sorted list
+        sorted_list.append(largest_transaction)
+
+        # remove largest transaction from the unsorted list
+        unsorted_list.remove(largest_transaction)
+
+    return(sorted_list)
